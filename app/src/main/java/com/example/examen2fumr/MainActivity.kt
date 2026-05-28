9d7200e5750389d3c609ad7b6e74e00ca3ddcc89
hexab1t3 <fer.uri.mi.ro@gmail.com>
2026-05-27 20:44:23 -0600

    Validación de contraseña

--- app/src/main/java/com/example/examen2fumr/MainActivity.kt
+++ app/src/main/java/com/example/examen2fumr/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.examen2fumr
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val password = etPassword.text.toString()
+
+            if (password == "abc123") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, getString(R.string.error_invalid_password), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
